90735d22030c2715cbfb8dae623421692051bd30
bguiz <brendan.graetz@gmail.com>
2014-09-26 18:33:01 +1000

    =BG= switch to broccoli-caching-writer to speed up incremental builds - sprite sheet only regenerated when input images have changed

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 var path = require('path');
 var mkdirp = require('mkdirp');
 var nodeSpriteGenerator = require('node-sprite-generator');
-var brocWriter = require('broccoli-writer');
+var brocCachingWriter  = require('broccoli-caching-writer');
 var rsvp = require('rsvp');
 
 var BroccoliSprite = function BroccoliSprite(inTree, options) {
   if (!(this instanceof BroccoliSprite)) {
     return new BroccoliSprite(inTree, options);
   }
-  this.inTree = inTree;
+  this.inputTree = inTree;
   options = options || {};
   this.options = {};
 
@@ -19,50 +19,50 @@ var BroccoliSprite = function BroccoliSprite(inTree, options) {
   }
 };
 
-BroccoliSprite.prototype = Object.create(brocWriter.prototype);
+//Using broccoli caching writer to avoid recompiling the sprite sheets each time
+BroccoliSprite.prototype = Object.create(brocCachingWriter.prototype);
 BroccoliSprite.prototype.constructor = BroccoliSprite;
 BroccoliSprite.prototype.description = 'sprite';
 BroccoliSprite.prototype.debugLog = function() {
   if (this.options.debug) {
-    console.log.apply(this, arguments);
+    console.log.apply(null, arguments);
   }
 };
-BroccoliSprite.prototype.write = function(readTree, destDir) {
-  var self = this;
-  self.debugLog('Running BroccoliSprite');
 
-  return readTree(this.inTree).then(function (srcDir) {
-    var files = (self.options.src || []).map(function(file) {
-      return srcDir+'/'+file;
-    });
-    self.debugLog('srcDir', srcDir);
-    self.debugLog('destDir', destDir);
-    self.debugLog('files', files);
-    var spritePath = path.join(destDir, self.options.spritePath);
-    var stylesheetPath = path.join(destDir, self.options.stylesheetPath);
-    mkdirp.sync(path.dirname(spritePath));
-    mkdirp.sync(path.dirname(stylesheetPath));
-    var nsgOptions = JSON.parse(JSON.stringify(self.options)); //lazy way to deep clone
-    nsgOptions.src = files;
-    nsgOptions.spritePath = spritePath;
-    nsgOptions.stylesheetPath = stylesheetPath;
-    self.debugLog('Options: ', nsgOptions);
-    self.debugLog('spritePath', spritePath);
-    self.debugLog('stylesheetPath', stylesheetPath);
-    var promise = new rsvp.Promise(function(resolvePromise, rejectPromise) {
-      nodeSpriteGenerator(nsgOptions, function (err) {
-          if (!err) {
-            self.debugLog('Sprite generated!');
-            resolvePromise(true);
-          }
-          else {
-            console.log('Sprite generation failed:', err);
-            rejectPromise(err);
-          }
-      });
-    });
-    return promise;
+BroccoliSprite.prototype.updateCache = function(srcDir, destDir) {
+  var self = this;
+  self.debugLog('Running BroccoliSprite updateCache');
+
+  var files = (self.options.src || []).map(function(file) {
+    return srcDir+'/'+file;
   });
+  self.debugLog('srcDir', srcDir);
+  self.debugLog('destDir', destDir);
+  self.debugLog('files', files);
+  var spritePath = path.join(destDir, self.options.spritePath);
+  var stylesheetPath = path.join(destDir, self.options.stylesheetPath);
+  mkdirp.sync(path.dirname(spritePath));
+  mkdirp.sync(path.dirname(stylesheetPath));
+  var nsgOptions = JSON.parse(JSON.stringify(self.options)); //lazy way to deep clone
+  nsgOptions.src = files;
+  nsgOptions.spritePath = spritePath;
+  nsgOptions.stylesheetPath = stylesheetPath;
+  self.debugLog('Options: ', nsgOptions);
+  self.debugLog('spritePath', spritePath);
+  self.debugLog('stylesheetPath', stylesheetPath);
+  var promise = new rsvp.Promise(function(resolvePromise, rejectPromise) {
+    nodeSpriteGenerator(nsgOptions, function (err) {
+        if (!err) {
+          self.debugLog('Sprite generated!');
+          resolvePromise(destDir);
+        }
+        else {
+          self.debugLog('Sprite generation failed:', err);
+          rejectPromise(err);
+        }
+    });
+  });
+  return promise;
 };
 
 module.exports = BroccoliSprite;
